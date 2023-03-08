3f01c7a34528b3af9e8cb078958871641f768462
mohd-fa <91002304+mohd-fa@users.noreply.github.com>
2023-03-08 21:51:17 +0530

    hello

--- stdmgnt/models.py
+++ stdmgnt/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
+class User(models.Model):
     userid = models.AutoField(primary_key=True)
     createdDate = models.DateTimeField(auto_now_add=True)
     updatedDate = models.DateTimeField(auto_now_add=True)
-    user_type_options = ((1,"admin"),(2,"student"),(3,"staff"))
-    user_type = models.CharField(default=1, choices=user_type_options,max_length=10)
+    user_type_options = ((1,"HOD"),(2,"Staff"),(3,"Student"))
+    user_type = models.IntegerField(default=1, choices=user_type_options)
 
-class department(models.Model):
+class Department(models.Model):
     name =  models.CharField(max_length=10)
     id = models.AutoField(primary_key=True)
     createdDate=models.DateField(auto_now_add=False)
@@ -20,20 +20,20 @@ class Staff(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    deptid = models.ForeignKey(department.id)
+    dept= models.ForeignKey(Department, on_delete=models.CASCADE)
     staffid = models.IntegerField(primary_key=True)
 
 class Student(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    deptid = models.ForeignKey(department.id)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
     admnno = models.IntegerField(primary_key=True)
-    semester = models.IntegerChoices(choices=range(1,9))
+    semester = models.IntegerField()
 
 class Subjects(models.Model):
     name = models.CharField(max_length=20)
     id = models.AutoField(primary_key=True)
-    departmentid = models.ForeignKey(department.id)
+    departmentid = models.ForeignKey(Department, on_delete=models.CASCADE)
     staff = models.ManyToManyField(Staff)
 
